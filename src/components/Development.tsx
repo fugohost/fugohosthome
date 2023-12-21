b830ffd617880c4d8e9e2623b43916ccbcb52859
fugo host <apoorva.rao@fugohost.com>
2023-12-21 14:10:50 -0500

    update content

--- src/components/Development.tsx
+++ src/components/Development.tsx
@@ -17,7 +17,14 @@ const Development = () => {
               We help build and manage a team of world-class developers to bring
               your vision to life.
             </p>
-            <a type="button" href="https://calendly.com/fugohost/30min?month=2023-11" target="_blank" rel="noreferrer">Let's get started!</a>
+            <a
+              type="button"
+              href="https://calendly.com/fugohost/30min?month=2023-11"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Let's get started!
+            </a>
           </div>
         </div>
 
@@ -27,53 +34,59 @@ const Development = () => {
       </div>
 
       <div className="web-design">
-        <div style={{backgroundColor: "#F1F2FF"}}>
+        <h1>Website Design Tailored For Your Requirement</h1>
+        <div style={{ backgroundColor: "#F1F2FF" }}>
           <div>
             <img src={image1} alt="case study image" />
           </div>
           <div>
-            <h3>Website Design for SCFC Canada</h3>
+            <h3>Design Catalog for FireFit App</h3>
             <p>
-              Born out of a vision, a single-minded objective that puts service
-              before anything else, Swift Clearance and Forwarding Corp. surging
-              forth to deliver the best services in the shipping and logistics
-              scenario. Its meteoric rise stems out of a solid foundation. The
-              management boasts of over 20 years of rich and varied experience
-              in the shipping and freight forwarding industry.
+              Create a visually appealing and informative catalog for the
+              FireFit app, an innovative fitness companion designed specifically
+              for firefighters. Emphasize the app's key features, including
+              tailored workouts, health tracking, nutritional guidance, and
+              community support. Highlight compatibility, download information,
+              and include contact details on the back cover for easy access.
             </p>
           </div>
         </div>
 
-        <div style={{backgroundColor: "#F0FFF7"}}>
+        <div style={{ backgroundColor: "#F0FFF7" }}>
           <div>
             <img src={image2} alt="case study image" />
           </div>
           <div>
-          <h3>Website Design for SCFC Canada</h3>
+            <h3>Design Catalog for MyGoals App</h3>
             <p>
-              Born out of a vision, a single-minded objective that puts service
-              before anything else, Swift Clearance and Forwarding Corp. surging
-              forth to deliver the best services in the shipping and logistics
-              scenario. Its meteoric rise stems out of a solid foundation. The
-              management boasts of over 20 years of rich and varied experience
-              in the shipping and freight forwarding industry.
+              Design a comprehensive catalog for the MyGoals app, a dynamic and
+              personalized goal-setting application. Highlight the app's key
+              features, such as intuitive goal tracking, progress visualization,
+              customizable goal categories, and motivational reminders. Showcase
+              the app's user interface through well-crafted screenshots and
+              graphics. Maintain a cohesive and modern design throughout the
+              catalog, utilizing a consistent color scheme and easily readable
+              fonts.
             </p>
           </div>
         </div>
 
-        <div  style={{backgroundColor: "#FFF4F4"}}>
+        <div style={{ backgroundColor: "#FFF4F4" }}>
           <div>
             <img src={image3} alt="case study image" />
           </div>
           <div>
-          <h3>Website Design for SCFC Canada</h3>
+            <h3>Design Catalog for Meditation App</h3>
             <p>
-              Born out of a vision, a single-minded objective that puts service
-              before anything else, Swift Clearance and Forwarding Corp. surging
-              forth to deliver the best services in the shipping and logistics
-              scenario. Its meteoric rise stems out of a solid foundation. The
-              management boasts of over 20 years of rich and varied experience
-              in the shipping and freight forwarding industry.
+              Create an engaging catalog for the Meditation Harmony app,
+              focusing on promoting inner peace and well-being through
+              mindfulness. Showcase the app's key features, including guided
+              meditation sessions, ambient sounds, personalized meditation
+              plans, and progress tracking. Use serene and calming visuals, such
+              as nature scenes and meditation settings, to evoke a sense of
+              tranquility. Let the catalog embody the essence of mindfulness and
+              encourage users to embark on a journey of self-discovery and
+              relaxation with Meditation Harmony.
             </p>
           </div>
         </div>
